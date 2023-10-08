437a34dfad35be9437fe822d3fc76c6d85b351b2
aprrrnl <ampurirer@tutanota.com>
2023-10-08 10:24:42 +0300

    add bug model

--- bug/models.py
+++ bug/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Bug(models.Model):
+    # A textual description of the bug
+    description = models.TextField()
+
+    # Choices for bug types
+    BUG_TYPES = [
+        ('error', 'Error'),
+        ('new_feature', 'New Feature'),
+        ('enhancement','Enhancement')
+    ]
+    bug_type = models.CharField(max_length=20, choices=BUG_TYPES)
+
+    # The date when the bug is being registered (automatically set to the current date and time)
+    report_date = models.DateTimeField(auto_now_add=True)
+
+    # Choices for bug status
+    STATUS_CHOICES = [
+        ('unconfirmed', 'Unconfirmed'),   
+        ('to_do', 'To Do'),
+        ('in_progress', 'In Progress'), 
+        ('done', 'Done'),
+        ('reopened', 'Reopened'),               
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.description
